870f324a73be4d1df61615fe464f664f106dfcb9
Adrián Gosálvez <agosalvez@alfatecsistemas.es>
2021-06-21 10:02:04 +0200

    prepare app for environment docker variables

---
+++ src/assets/env.template.js
@@ -0,0 +1,8 @@
+(function(window) {
+    window.env = window.env || {};
+
+    // Environment variables
+    window["env"]["trexUrl"] = "${TREX_URL}";
+    window["env"]["keycloakUrl"] = "${MOGES_URL}";
+    window["env"]["keycloakRealm"] = "${PROFILE}";
+})(this);
