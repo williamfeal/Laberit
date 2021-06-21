42cc0e22f95a6a73316946c0ee3385638d8bde98
syanez <syanez@alfatecsistemas.es>
2021-06-21 10:52:19 +0200

    feat(header): Add date

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as DateConstants from 'src/app/utils/constants/date-constants';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public currentDateHeader = '';
+  public currentHourHeader = '';
+
+  private currentDate = new Date();
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.loadDate();
+    this.loadHour();
+    setInterval(this.loadHour, 1000);
   }
+
+  private loadDate(): void {
+    this.currentDateHeader = DateConstants.weekDays[this.currentDate.getDay()-1] + ' ' + this.currentDate.getDate() + ' de ' +
+      DateConstants.monthList[this.currentDate.getMonth()] + ' de ' + this.currentDate.getFullYear();
+  }
+
+  private loadHour(): void {
+    const currentDate = new Date();
+    const minutes = (currentDate.getMinutes() < 10 ? '0' : '') + currentDate.getMinutes();
+    this.currentHourHeader = currentDate.getHours() + ':' + minutes + 'h';
+    document.getElementById("horas").innerHTML = this.currentHourHeader;
+
+  }
+
 
 }
