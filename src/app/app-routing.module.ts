fec81145889c9956ca98cf42bc1a49285cd6836e
syanez <syanez@alfatecsistemas.es>
2021-06-21 11:45:49 +0200

    feat(generalSEDE): Slider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CarpetaCiudadanaComponent } from './components/carpeta-ciudadana/carpeta-ciudadana.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
-  { path: 'web/guest/carpeta-del-ciudadano', component: CarpetaCiudadanaComponent }
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'web/guest/carpeta-del-ciudadano', component: CarpetaCiudadanaComponent },
+  { path: 'inicio', component: HomeComponent}
 ];
 
 @NgModule({
