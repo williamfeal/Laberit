478794317646e8fd030cab523904efa7b792ff36
Pablo García Planells <pgarciap@alfatecsistemas.es>
2021-06-22 12:31:24 +0200

    factura electronica

--- src/app/components/electronic-services/electronic-invoice/electronic-invoice.component.ts
+++ src/app/components/electronic-services/electronic-invoice/electronic-invoice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-electronic-invoice',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElectronicInvoiceComponent implements OnInit {
 
-  constructor() { }
+  title = '';
+  text = '';
+  button = '';
+  link = 'https://face.gob.es/es';
+  enviar = false;
+
+  constructor(public translate: TranslateService) {
+    this.translate.get('electronic-services.electronic-invoice').subscribe((texts: any) => {
+      this.text = texts.text;
+      this.title = texts.title;
+      this.button = texts.button;
+      this.enviar = true;
+    })
+  }
 
   ngOnInit(): void {
   }
