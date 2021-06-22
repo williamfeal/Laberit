2ac4d7fedb4c1eb5dca79f0d1bc00b30b85a25dd
Pablo García Planells <pgarciap@alfatecsistemas.es>
2021-06-22 12:29:06 +0200

    crear rutas y traduccion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdvertisementsListComponent } from './components/advertisements-list/advertisements-list.component';
 import { CarpetaCiudadanaComponent } from './components/carpeta-ciudadana/carpeta-ciudadana.component';
 import { ContractorProfileComponent } from './components/contractor-profile/contractor-profile.component';
+import { DocumentVerificationComponent } from './components/electronic-services/document-verification/document-verification.component';
+import { ElectronicInvoiceComponent } from './components/electronic-services/electronic-invoice/electronic-invoice.component';
+import { NotificationsComponent } from './components/electronic-services/notifications/notifications.component';
+import { ValideComponent } from './components/electronic-services/valide/valide.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
@@ -10,7 +14,11 @@ const routes: Routes = [
   { path: 'carpeta-del-ciudadano', component: CarpetaCiudadanaComponent },
   { path: 'tablon-de-anuncios', component: AdvertisementsListComponent },
   { path: 'inicio', component: HomeComponent},
-  { path: 'perfil-de-contratante', component: ContractorProfileComponent}
+  { path: 'perfil-de-contratante', component: ContractorProfileComponent},
+  { path: 'validacion', component: ValideComponent},
+  { path: 'factura-electronica', component: ElectronicInvoiceComponent},
+  { path: 'verifirma', component: DocumentVerificationComponent},
+  { path: 'notificaciones', component: NotificationsComponent},
 ];
 
 @NgModule({
