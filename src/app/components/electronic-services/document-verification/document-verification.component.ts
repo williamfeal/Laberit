b0e0208c344d84524d3475eb83e576e33f13eecf
Pablo García Planells <pgarciap@alfatecsistemas.es>
2021-06-22 12:32:41 +0200

    verificacion de docuemntes pre-captcha(falta captcha)

--- src/app/components/electronic-services/document-verification/document-verification.component.ts
+++ src/app/components/electronic-services/document-verification/document-verification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-document-verification',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentVerificationComponent implements OnInit {
 
-  constructor() { }
+  title = '';
+  text = '';
+  button = '';
+  link = 'https://face.gob.es/es';
+  enviar = false;
+
+  constructor(public translate: TranslateService) {
+    
+  }
 
   ngOnInit(): void {
+    this.translate.get('electronic-services.document-verification').subscribe((texts: any) => {
+      this.title = texts.title;
+      let text = document.getElementById("text");
+      let csv = document.getElementById("placeholder_csv");
+      let img = document.getElementById("placeholder_img");
+      let button = document.getElementById("button");
+      let where_text = document.getElementById("where_text");
+      let where_title = document.getElementById("where_title");
+      text.innerHTML = texts.text;
+      button.innerHTML = texts.button;
+      where_title.innerHTML = texts.where_title;
+      where_text.innerHTML = texts.where_text;
+      button.setAttribute("href", this.link);
+      csv.setAttribute("placeholder", texts.placeholder_csv)
+      img.setAttribute("placeholder", texts.placeholder_img)
+      this.enviar = true;
+    })
   }
 
 }
