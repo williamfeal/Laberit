caacc3fcef7303d9637c06e88000b97c41684f52
Pablo García Planells <pgarciap@alfatecsistemas.es>
2021-06-24 14:49:03 +0200

    feat(General-SEDE): csv and captcha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { ProcedimentsHomeComponent } from './components/home/procediments-home/p
 import { CarpetaCiudadanaComponent } from './components/carpeta-ciudadana/carpeta-ciudadana.component';
 import { HomeComponent } from './components/home/home.component';
 import { AdvertisementsListComponent } from './components/advertisements-list/advertisements-list.component';
-import { HeaderCategoryComponent } from './shared/header/header-category/header-category.component';
+import { HeaderCategoryComponent } from './shared/header-category/header-category.component';
 import { ProcedimentsComponent } from './components/procediments/procediments.component';
 
 /* Shared */
@@ -42,7 +42,8 @@ import { GenericStaticViewComponent } from './shared/generic-static-view/generic
 import { CategoriesService } from './services/moges-services/categories.service';
 import { FooterComponent } from './shared/footer/footer.component';
 import { AdvertisementsService } from './services/moges-services/advertisements.service';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxCaptchaModule } from 'ngx-captcha';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
@@ -78,6 +79,8 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
     CommonModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    NgxCaptchaModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
