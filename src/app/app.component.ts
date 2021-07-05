04dfbbd535882f119a7b274f8dc430cf29daf374
syanez <syanez@alfatecsistemas.es>
2021-07-05 13:09:37 +0200

    feat(styles): Sonar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { StyleService } from './services/moges-services/style.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
