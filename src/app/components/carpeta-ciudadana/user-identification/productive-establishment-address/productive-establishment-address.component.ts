340b943b286e8459eee3eeb37b63cf9adcddf4c6
syanez <syanez@alfatecsistemas.es>
2021-07-22 10:09:12 +0200

    feat(forms): data generic fields

--- src/app/components/carpeta-ciudadana/user-identification/productive-establishment-address/productive-establishment-address.component.ts
+++ src/app/components/carpeta-ciudadana/user-identification/productive-establishment-address/productive-establishment-address.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-productive-establishment-address',
   templateUrl: './productive-establishment-address.component.html'
 })
 export class ProductiveEstablishmentAddressComponent implements OnInit {
+
+  @Input() formProductiveEstablishment:FormGroup;
+  @Input() readOnly:boolean;
 
   constructor() { }
 
