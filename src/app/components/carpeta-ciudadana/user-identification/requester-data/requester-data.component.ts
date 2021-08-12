c434d3f86fd6073def2341ad00c526e0e0cf8fd5
dani <dsoler@alfatecsistemas.es>
2021-08-12 13:24:36 +0200

    feat: validation selected interested representante

--- src/app/components/carpeta-ciudadana/user-identification/requester-data/requester-data.component.ts
+++ src/app/components/carpeta-ciudadana/user-identification/requester-data/requester-data.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { SelectFieldObject } from 'src/app/shared/form/fields/input-select/input-select';
 import { actuation } from 'src/app/utils/constants/app-constants';
 
@@ -8,6 +9,9 @@ import { actuation } from 'src/app/utils/constants/app-constants';
 })
 export class RequesterDataComponent implements OnInit {
 
+  @Input() formRequestData: FormGroup;
+  @Input() validate: boolean;
+  errorCharacterLeng: string = 'num_Characters_error';
   public type = 'interested'; ///ES POSIBLE QUE FALTE AÑADIR EL FORM
   public actuation: SelectFieldObject[];
   @Output() public typeOutput = new EventEmitter<string>();
