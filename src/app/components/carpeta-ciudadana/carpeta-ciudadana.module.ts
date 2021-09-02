17dbc1ec8a83077b4b2320bb2a7ac0f7ef505bd1
Pablo García Planells <pgarciap@alfatecsistemas.es>
2021-09-02 12:14:56 +0200

    clean code

--- src/app/components/carpeta-ciudadana/carpeta-ciudadana.module.ts
+++ src/app/components/carpeta-ciudadana/carpeta-ciudadana.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DocumentationComponent } from './documentation/documentation.component';
 import { CarpetaCiudadanaComponent } from './carpeta-ciudadana.component';
 import { CarpetaService } from 'src/app/services/trex-service/carpeta.service';
 import { AdjuntarDocComponent } from './includes/adjuntar-documento/adjuntar-doc.component';
@@ -36,9 +35,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    DocumentationComponent,
     CarpetaCiudadanaComponent,
-    DocumentationComponent,
     AdjuntarDocComponent,
     ConfirmacionSolicitudComponent,
     DateComponent,
