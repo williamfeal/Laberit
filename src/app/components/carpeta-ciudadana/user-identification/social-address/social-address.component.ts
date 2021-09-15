f4f3e4555f2a08b8f84da9733c95a9f5ab356c21
syanez <syanez@alfatecsistemas.es>
2021-09-15 13:19:50 +0200

    FEAT(catalogs): social data catalogs

--- src/app/components/carpeta-ciudadana/user-identification/social-address/social-address.component.ts
+++ src/app/components/carpeta-ciudadana/user-identification/social-address/social-address.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { CatalogsService } from 'src/app/services/catalogs/catalogs.service';
 import { SelectFieldObject } from 'src/app/shared/form/fields/input-select/input-select';
 import { provincias, typeStreet, paises, comunidades } from 'src/app/utils/constants/app-constants';
+import { ConceptConstants } from 'src/app/utils/constants/concept-constants';
 
 @Component({
   selector: 'app-social-address',
@@ -13,15 +15,58 @@ export class SocialAddressComponent implements OnInit {
   @Input() validate: boolean;
   @Input() isRequired: boolean;
 
-  public provincias: SelectFieldObject[] = provincias;
-  public municipios: SelectFieldObject[] = comunidades;
-  public paises: SelectFieldObject[] = paises;
+  public provincias: SelectFieldObject[];
+  public municipios: SelectFieldObject[];
+  public paises: SelectFieldObject[];
+  public typeStreet: SelectFieldObject[];
 
-errorCharacterLeng: string = 'empty_error';
-typeStreet = typeStreet;
-constructor() { }
+  public countrySpainSelected;
 
-ngOnInit(): void {
-}
+  errorCharacterLeng: string = 'empty_error';
+  
+  constructor(
+    private catalogService:CatalogsService
+  ) { }
+
+  ngOnInit(): void {
+    this.getRoadTypes();
+    this.getCountries();
+  }
+
+  public getRoadTypes() {
+    this.catalogService.getCatalogByCode(ConceptConstants.ROAD_TYPES).subscribe(
+      data => this.typeStreet = data.sort(function(a, b){
+        if(a.description < b.description) { return -1; }
+        if(a.description > b.description) { return 1; }
+        return 0;
+    })
+    )
+  }
+
+  public getCountries() {
+    this.catalogService.getCatalogByCode(ConceptConstants.COUNTRIES).subscribe(
+      data => {
+        this.paises = data
+        this.getSpainCountries()
+      }
+    )
+  }
+
+  public getSpainCountries() {
+    this.catalogService.getCatalogByCode(ConceptConstants.COUNTRIES_SPAIN).subscribe(
+      data => this.provincias = data.sort(function(a, b){
+        if(a.description < b.description) { return -1; }
+        if(a.description > b.description) { return 1; }
+        return 0;
+    })
+    )
+  }
+
+  public onChangeSpainCountry(event) {
+    this.catalogService.getCatalogByCode(event).subscribe(
+      data => this.municipios = data,
+      () => this.municipios = []
+    )
+  }
 
 }
