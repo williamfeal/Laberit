1f80eb74e8665eba24aa5c1cce94e95617daa7f0
syanez <syanez@alfatecsistemas.es>
2021-09-16 11:49:56 +0200

    fix(language): change va to ca

--- src/app/utils/pipes/localized-date.pipe.ts
+++ src/app/utils/pipes/localized-date.pipe.ts
@@ -13,7 +13,7 @@ export class LocalizedDatePipe implements PipeTransform {
 
   transform(value: any, pattern: string = 'mediumDate'): any {
     const lang =
-      this.translateService.currentLang === 'va' ? 'ca-ES' : this.translateService.currentLang;
+      this.translateService.currentLang === 'ca' ? 'ca-ES' : this.translateService.currentLang;
     const datePipe: DatePipe = new DatePipe(lang);
     const firstLettter = datePipe.transform(value, pattern).substr(0,1).toUpperCase();
     return firstLettter + datePipe.transform(value, pattern).substr(1);
