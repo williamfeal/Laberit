f93f70999ecafaf1e820080ad74dde83852d1904
syanez <syanez@alfatecsistemas.es>
2021-10-26 11:31:19 +0200

    fix/button-back: fix imports

--- src/app/components/carpeta-ciudadana/tramites/linea-resistir/linea-resistir.component.ts
+++ src/app/components/carpeta-ciudadana/tramites/linea-resistir/linea-resistir.component.ts
@@ -2,19 +2,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { BusinessRule } from './../../../../models/business-rules.model';
 import { BusinessRuleBody } from './../../../../models/business-rules-body.model';
 import { BusinessRulesService } from './../../../../services/acli-service/business-rules.service';
-import { CarpetaService } from 'src/app/services/acli-service/carpeta.service';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ConceptConstants } from 'src/app/utils/constants/concept-constants';
 import { Decision } from './../../../../models/decision.model';
 import { Draft } from 'src/app/models/draft.model';
 import { DraftsService } from './../../../../services/acli-service/drafts.service';
-import {
-    FormBuilder,
-    FormControl,
-    FormGroup,
-    Validators
-    } from '@angular/forms';
-import { Location } from '@angular/common';
+import { FormGroup } from '@angular/forms';
 import { Procedure } from 'src/app/models/procedure.model';
 import { ProceduresService } from 'src/app/services/moges-services/procedures.service';
 import { Subject } from 'rxjs';
@@ -51,7 +44,6 @@ export class LineaResistirComponent implements OnInit {
     constructor(
         private activatedRoute:ActivatedRoute,
         private router: Router,
-        private location:Location,
         private ref: ChangeDetectorRef,
         private procedureService:ProceduresService,
         private translate:TranslateService,
