4f9ff3d0235494921eea3801552efc7dff2ff501
dani <dsoler@alfatecsistemas.es>
2021-11-15 15:57:01 +0100

    feat: plantillas

--- src/app/services/attachDocs/atach.service.ts
+++ src/app/services/attachDocs/atach.service.ts
@@ -12,8 +12,7 @@ export class atachService {
     private URL_API_CATALOGS = environment.catalogs_url + UrlConstants.ENDPOINT_CONCEPTS;
     private URL_GET_CONCEPT_BY_CODE = this.URL_API_CATALOGS + UrlConstants.ENDPOINT_GET_CONCEPT_BY_CODE;
     private URL_GET_CATALOG_BY_CODE = this.URL_API_CATALOGS + UrlConstants.ENDPOINT_GET_CATALOG_BY_CODE;
-    private urlAlfresco = UrlConstants.URL_ALFRESCO_ATTACH_DOC;
-    private urlAlfrescoDelete = UrlConstants.URL_ALFRESO_DELETE_DOC;
+    
     public httpOptions = {
         headers: new HttpHeaders({
           'Content-Type':  'application/json',
@@ -25,40 +24,8 @@ export class atachService {
         private http:HttpClient
       ){}
 
-    public attachDocument(dataBody){
-      const databodyUrl = {
-            fileData: dataBody,
-            autoRemane: true,
-            relativePath: "/attachDocuments",
-        };
-        
-    const data = this.http.post(`${this.urlAlfresco}`, databodyUrl , this.httpOptions);
-    return data.pipe(map((response:any) => {
-      return response.data;
-    })).pipe(catchError(error => {
-      let errorMsg: string;
-      if (error.error instanceof ErrorEvent) {
-          errorMsg = `Error: ${error.error.message}`;
-      } else {
-          errorMsg = this.getServerErrorMessage(error);
-      }
-      return throwError(errorMsg);
-  })); 
-    } 
-    public deleteDocument(idDoc){
-        const data = this.http.post(`${this.urlAlfrescoDelete}`+idDoc+'?permanent=false' , this.httpOptions);
-    return data.pipe(map((response:any) => {
-      return response.data;
-    })).pipe(catchError(error => {
-      let errorMsg: string;
-      if (error.error instanceof ErrorEvent) {
-          errorMsg = `Error: ${error.error.message}`;
-      } else {
-          errorMsg = this.getServerErrorMessage(error);
-      }
-      return throwError(errorMsg);
-    })); 
-    }
+   
+   
 
     private getServerErrorMessage(error: HttpErrorResponse): string {
         switch (error.status) {
