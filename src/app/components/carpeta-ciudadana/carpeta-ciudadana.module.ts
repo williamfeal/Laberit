dc9134c18b0987a7abbd1b7482ee5251ffb86a45
syanez <syanez@alfatecsistemas.es>
2021-12-13 10:47:19 +0100

    FIX/representa: Fix poder representacion

--- src/app/components/carpeta-ciudadana/carpeta-ciudadana.module.ts
+++ src/app/components/carpeta-ciudadana/carpeta-ciudadana.module.ts
@@ -2,6 +2,7 @@ import { AceptationComponent } from './aceptation/aceptation.component';
 import { AdjuntarDocComponent } from './includes/adjuntar-documento/adjuntar-doc.component';
 import { AutonomsComponent } from './includes/adjuntar-documento/autonoms/autonoms.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BussinesType } from './user-identification/representative-data/dialog-bussinesType/bussinesType.component';
 import { CarpetaCiudadanaComponent } from './carpeta-ciudadana.component';
 import { CarpetaCiudadanaRoutingModule } from './carpeta-ciudadana-routing.module';
 import { CarpetaService } from 'src/app/services/acli-service/carpeta.service';
@@ -14,6 +15,7 @@ import { DateComponent } from './includes/date-component/date.component';
 import { DatosInteresadoComponent } from './includes/datos-interesado/datos-interesado.component';
 import { DatosNotificacionComponent } from './includes/datos-notificacion/datos-notificacion.component';
 import { DetailCostFinancedComponent } from './tramites/linea-resistir/detail-cost-financed/detail-cost-financed.component';
+import { EdicionSolicitudComponent } from './requests-list/edicion-terceros/edicion-solicitud.component';
 import { FirmarYPresentarPopUp } from './includes/firmarYpresentarPopUp/firmarYpresentarPopUp.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GranEmpresaComponent } from './includes/adjuntar-documento/granEmpresa/granEmpresa.component';
@@ -23,13 +25,17 @@ import { InstanciaGeneralComponent } from './tramites/instancia-general/instanci
 import { InterestedDataComponent } from './user-identification/interested-data/interested-data.component';
 import { LegalRepresentativeComponent } from './user-identification/legal-representative/legal-representative.component';
 import { LineaResistirComponent } from './tramites/linea-resistir/linea-resistir.component';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
 import { MicroEmpresaComponent } from './includes/adjuntar-documento/microempresa/microEmpresa.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { NotificationMeansComponent } from './user-identification/notification-means/notification-means.component';
 import { NotificationsListComponent } from './notifications/notifications-list/notifications-list.component';
+import { NotificationViewComponent } from './notifications/notification-view/notification-view.component';
+import { PoderRepresentacionComponent } from './user-identification/comprobar-poder-rep/poder-representacion.component';
 import { PresentarSolicitudComponent } from './includes/presentar-solicitud/presentar-solicitud.component';
 import { ProcedureInformationComponent } from './tramites/linea-resistir/procedure-information/procedure-information.component';
 import { ProductiveEstablishmentAddressComponent } from './user-identification/productive-establishment-address/productive-establishment-address.component';
@@ -38,18 +44,12 @@ import { RepresentativeDataComponent } from './user-identification/representativ
 import { RequesterDataComponent } from './user-identification/requester-data/requester-data.component';
 import { RequestsListComponent } from './requests-list/requests-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SignNotificationReceiptComponent } from './notifications/sign-notification-receipt/sign-notification-receipt.component';
 import { SocialAddressComponent } from './user-identification/social-address/social-address.component';
 import { SociedadCivilComponent } from './includes/adjuntar-documento/sociedadCivil/sociedadCivil.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { UserIdentificationComponent } from './user-identification/user-identification.component';
-import { NotificationViewComponent } from './notifications/notification-view/notification-view.component';
-import { SignNotificationReceiptComponent } from './notifications/sign-notification-receipt/sign-notification-receipt.component';
-import { PoderRepresentacionComponent } from './tramites/comprobar-poder-rep/poder-representacion.component';
-import { BussinesType } from './user-identification/representative-data/dialog-bussinesType/bussinesType.component';
-import { MatDialogModule } from '@angular/material/dialog';
 
-import { MatStepperModule } from '@angular/material/stepper';
-import { EdicionSolicitudComponent } from './requests-list/edicion-terceros/edicion-solicitud.component';
 
 
 @NgModule({
